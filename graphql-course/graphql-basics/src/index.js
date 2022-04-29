b40a5cdaf8bb48d6ccc30239823496676b9696f5
Yongjae Kim <mearak43@gmail.com>
2022-04-30 01:40:40 +0900

    keep going! 11-14, section2, graphql course

--- graphql-course/graphql-basics/src/index.js
+++ graphql-course/graphql-basics/src/index.js
@@ -1,5 +1,55 @@
-import defaultSomething, { message, getGreeting } from "./myModule";
+import { GraphQLServer } from "graphql-yoga";
 
-console.log("message", message);
-console.log("default", defaultSomething);
-console.log(getGreeting("jay"));
+// Scalar types - String, Boolean, Int, Float, ID
+
+// Type definition(Schema)
+const typeDefs = `
+   type Query {
+     me: User! 
+     post: Post!
+   }
+
+   type User {
+       id: ID!
+       name: String!
+       email: String!
+       age: Int
+   }
+
+   type Post {
+       id: ID!
+       title: String!
+       body: String!
+       published: Boolean!
+   }
+`;
+// Resolvers
+const resolvers = {
+  Query: {
+    me() {
+      return {
+        id: "abc1234",
+        name: "Jay Kim",
+        email: "hello@google.com",
+        age: 30,
+      };
+    },
+    post() {
+      return {
+        id: "abc1234",
+        title: "Welcome to GraphQL!",
+        body: "Let's Learn GraphQL!",
+        published: true,
+      };
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => {
+  console.log("server is working!");
+});
--- graphql-course/graphql-basics/src/myModule.js
+++
@@ -1,7 +0,0 @@
-const message = "Some message from myModule.js";
-const defaultMessage = "this is default!!";
-const getGreeting = (name) => {
-  return `Hello ${name}!!`;
-};
-
-export { message, defaultMessage as default, getGreeting };
