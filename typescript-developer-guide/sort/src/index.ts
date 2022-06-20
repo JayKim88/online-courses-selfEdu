a0fe25f923b76fa3041684897b4aa473db3d0495
Yongjae Kim <mearak43@gmail.com>
2022-06-20 23:46:42 +0900

    learn: 88 - 94, section10, typescript

--- typescript-developer-guide/sort/src/index.ts
+++ typescript-developer-guide/sort/src/index.ts
@@ -1,7 +1,24 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+// const charactersCollection = new CharactersCollection("cayXb");
+
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(23);
+linkedList.add(55);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(numbersCollection.data);
+linkedList.print();
