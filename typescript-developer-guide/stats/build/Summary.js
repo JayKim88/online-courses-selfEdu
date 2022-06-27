2aab59b4dc0621dd805b75d74e10b214552d3ed2
Yongjae Kim <mearak43@gmail.com>
2022-06-27 11:38:14 +0900

    learn: 126-140, section 12, ts-dev-guide

---
+++ typescript-developer-guide/stats/build/Summary.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Summary = void 0;
+const HtmlReport_1 = require("./reportTargets/HtmlReport");
+const WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
+class Summary {
+    constructor(analyzer, outputTarget) {
+        this.analyzer = analyzer;
+        this.outputTarget = outputTarget;
+    }
+    static winsAnalysisWithHtmlReport(team) {
+        return new Summary(new WinsAnalysis_1.WinsAnalysis(team), new HtmlReport_1.HtmlReport());
+    }
+    buildAndPrintReport(matches) {
+        const output = this.analyzer.run(matches);
+        console.log(output);
+        this.outputTarget.print(output);
+    }
+}
+exports.Summary = Summary;
