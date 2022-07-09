320e6ddf6243cc86381dc915f522e87c5b34ab4b
Yongjae Kim <mearak43@gmail.com>
2022-07-10 05:42:29 +0900

    learn: 260-284, section 17,18, complete ts-dev-guide

---
+++ typescript-developer-guide/rrts/src/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { applyMiddleware } from "redux";
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { App } from "./components/App";
+import { reducers } from "./reducers";
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.querySelector("#root")
+);
