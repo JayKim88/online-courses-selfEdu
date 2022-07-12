7d80da233011242e5afc65a28cf8c4b3ce396e99
Yongjae Kim <mearak43@gmail.com>
2022-07-13 06:18:27 +0900

    learn: complete section4, react-testing-jest-rtl

--- react-testing-jest-rtl/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ react-testing-jest-rtl/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,12 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  fireEvent,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+// userEvent.click not working properly at first clicking action
+import userEvent from "@testing-library/user-event";
 
 test("Initial conditions", () => {
   render(<SummaryForm />);
@@ -15,7 +22,7 @@ test("Initial conditions", () => {
 test("Checkbox enables button on first click and disables on second click", () => {
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
-    name: /terms and condition/i,
+    name: /terms and conditions/i,
   });
   const confirmButton = screen.getByRole("button", { name: /confirm order/i });
 
@@ -24,4 +31,27 @@ test("Checkbox enables button on first click and disables on second click", () =
 
   fireEvent.click(checkbox);
   expect(confirmButton).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  render(<SummaryForm />);
+
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+
+  expect(nullPopover).not.toBeInTheDocument();
+
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+
+  userEvent.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+
+  expect(popover).toBeInTheDocument();
+
+  userEvent.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i)
+  );
 });
