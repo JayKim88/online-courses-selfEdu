7bb313803613848b5e2dc92ba53feff60bc40960
Yongjae Kim <mearak43@gmail.com>
2022-07-13 05:22:12 +0900

    Test and code summary form, react-testing-jest-rtl

--- react-testing-jest-rtl/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ react-testing-jest-rtl/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,27 +1,27 @@
-import { fireEvent, render, screen } from "@testing-library/react"
-import SummaryForm from "../SummaryForm"
+import { fireEvent, render, screen } from "@testing-library/react";
+import SummaryForm from "../SummaryForm";
 
 test("Initial conditions", () => {
-    render(<SummaryForm/>)
-    const checkbox= screen.getByRole('checkbox',{
-        name: /terms and condition/i,
-    }) 
-    expect(checkbox).not.toBeChecked();
-    
-    const confirmButton = screen.getByRole('button',{name: /confirm order/i})
-    expect(confirmButton).toBeDisabled() 
-})
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and condition/i,
+  });
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+  expect(confirmButton).toBeDisabled();
+});
 
 test("Checkbox enables button on first click and disables on second click", () => {
-    render(<SummaryForm/>)
-    const checkbox= screen.getByRole('checkbox',{
-        name: /terms and condition/i,
-    }) 
-    const confirmButton = screen.getByRole('button',{name: /confirm order/i})
-    
-    fireEvent.click(checkbox)
-    expect(confirmButton).toBeEnabled();
-    
-    fireEvent.click(checkbox)
-    expect(confirmButton).toBeDisabled();
-})
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and condition/i,
+  });
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeEnabled();
+
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeDisabled();
+});
