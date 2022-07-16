ea3292c825f206a09be15ad904107b154ecc6409
Yongjae Kim <mearak43@gmail.com>
2022-07-16 11:37:17 +0900

    test and code alert for server error response, react-testing-jest-rtl

--- react-testing-jest-rtl/sundaes-on-demand/src/pages/entry/Options.jsx
+++ react-testing-jest-rtl/sundaes-on-demand/src/pages/entry/Options.jsx
@@ -1,18 +1,28 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Row } from "react-bootstrap";
+import AlertBanner from "../common/AlertBanner";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
-      .then((res) => setItems(res.data))
-      .catch((err) => {});
+      .then((res) => {
+        setItems(res.data);
+      })
+      .catch((err) => {
+        setError(true);
+      });
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
