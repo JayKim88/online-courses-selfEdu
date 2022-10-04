a93c3c9503d8cef1ab709243308d2277809dfef9
Yongjae Kim <mearak43@gmail.com>
2022-10-05 01:04:42 +0900

    learn: section4 graphql basics subscriptions -37, the-modern-graphql-bootcamp

--- graphql-course/graphql-basics/src/resolvers/Mutation.js
+++ graphql-course/graphql-basics/src/resolvers/Mutation.js
@@ -62,7 +62,7 @@ const Mutation = {
 
     return user;
   },
-  createPost(parent, args, { db }, info) {
+  createPost(parent, args, { db, pubsub }, info) {
     const userExists = db.users.some((user) => user.id === args.data.author);
 
     if (!userExists) {
@@ -75,6 +75,16 @@ const Mutation = {
     };
 
     db.posts.push(post);
+
+    if (args.data.published) {
+      console.log("hey");
+      pubsub.publish("post", {
+        post: {
+          mutation: "CREATED",
+          data: post,
+        },
+      });
+    }
 
     return post;
   },
@@ -94,20 +104,29 @@ const Mutation = {
     }
     return post;
   },
-  deletePost(parent, args, { db }, info) {
+  deletePost(parent, args, { db, pubsub }, info) {
     const postIndex = db.posts.findIndex((post) => post.id === args.id);
 
     if (postIndex === -1) {
       throw new Error("Post not found");
     }
 
-    const deletedPosts = db.posts.splice(postIndex, 1);
+    const [post] = db.posts.splice(postIndex, 1);
 
     db.comments = db.comments.filter((comment) => comment.post !== args.id);
 
-    return deletedPosts[0];
+    if (post.published) {
+      pubsub.publish("post", {
+        post: {
+          mutation: "DELETED",
+          data: post,
+        },
+      });
+    }
+
+    return post;
   },
-  createComment(parent, args, { db }, info) {
+  createComment(parent, args, { db, pubsub }, info) {
     const userExists = db.users.some((user) => user.id === args.data.author);
     const postExists = db.posts.some(
       (post) => post.id === args.data.post && post.published
@@ -123,6 +142,7 @@ const Mutation = {
     };
 
     db.comments.push(comment);
+    pubsub.publish(`comment ${args.data.post}`, { comment });
 
     return comment;
   },
