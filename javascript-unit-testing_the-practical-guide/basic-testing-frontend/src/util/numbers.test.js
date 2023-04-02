1f41e2b7b35e948d3dacc6af38582ba66d2f6808
Yongjae Kim <mearak43@gmail.com>
2023-04-02 11:22:34 +0900

    javascript-unit-testing: section4 writing good tests

---
+++ javascript-unit-testing_the-practical-guide/basic-testing-frontend/src/util/numbers.test.js
@@ -0,0 +1,27 @@
+import { describe, it, expect } from "vitest";
+
+import { transformToNumber } from "./numbers.js";
+
+it("should transform a string number to a number of type number", () => {
+  const input = "1";
+
+  const result = transformToNumber(input);
+
+  expect(result).toBeTypeOf("number");
+});
+
+it("should transform a string number to a number of type number", () => {
+  const input = "1";
+
+  const result = transformToNumber(input);
+
+  expect(result).toBe(+input);
+});
+
+it("should yield NaN for non-transformable values", () => {
+  const input = "invalid";
+
+  const result = transformToNumber(input);
+
+  expect(result).toBeNaN();
+});
